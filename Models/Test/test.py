620977c91c35654d6d6a983abed0ead13cbe2dd0
JusteRaimbault <raimbaultjwin@gmail.com>
2016-02-01 16:11:10 +0100

    test_data_collection

---
+++ Models/Test/test.py
@@ -0,0 +1,12 @@
+# test for google directions api request
+
+# url https://maps.googleapis.com/maps/api/directions/json?origin=48.8780829,2.2806976&destination=48.8782716,2.4084414
+
+import requests
+
+conf = open(file,'r')
+key = conf.readline().replace('\n','')
+result = requests.get('https://maps.googleapis.com/maps/api/directions/json?origin=48.8780829,2.2806976&destination=48.8782716,2.4084414&departure_time=now&traffic_model=best_guess&key='+key)
+data = result.json()
+
+print(data['routes'][0]['legs'][0]['duration_in_traffic']['value'])
