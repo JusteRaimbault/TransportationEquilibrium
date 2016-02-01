668abe28f983d9b3f17cdd1ce90ddc8c20695e8d
JusteRaimbault <juste.raimbault@polytechnique.edu>
2016-02-01 19:30:34 +0100

    maps_traffic

--- Models/Test/test.py
+++ Models/Test/test.py
@@ -4,9 +4,12 @@
 
 import requests
 
-conf = open(file,'r')
+conf = open('conf/key','r')
 key = conf.readline().replace('\n','')
+#print(key)
 result = requests.get('https://maps.googleapis.com/maps/api/directions/json?origin=48.8780829,2.2806976&destination=48.8782716,2.4084414&departure_time=now&traffic_model=best_guess&key='+key)
 data = result.json()
+
+#print(data)
 
 print(data['routes'][0]['legs'][0]['duration_in_traffic']['value'])
