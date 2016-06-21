10b4c8998101e5b7935f65f593dc38883cd5f2a8
JusteRaimbault <raimbaultjwin@gmail.com>
2016-06-21 23:23:57 +0200

    gmaps_random

--- Models/Test/setup.py
+++ Models/Test/setup.py
@@ -2,11 +2,18 @@
 # setup sqlite database for sytadin traffic collection
 import sqlite3
 
-def setup():
+def setup_sytadin():
     conn = sqlite3.connect('data/sytadin.sqlite3')
     c = conn.cursor()
     c.execute('CREATE TABLE data (troncon text, tps_th real, tps real, confiance real, ts real);')
     conn.commit()
     conn.close()
 
-setup()
+def setup_gmaps():
+    conn = sqlite3.connect('data/gmaps.sqlite3')
+    c = conn.cursor()
+    c.execute('CREATE TABLE data (ts real, id real, tps real);')
+    conn.commit()
+    conn.close()
+
+setup_gmaps()
